5f4bc143525b3840ca8dbf55e3523e85e75de633
Mike Lynch <mike.lynch@iinet.net.au>
2014-02-19 14:45:08 +1100

    Release 1.1 - Herbig-Haro object First release of parameter scatterplots

--- js/starmap.js
+++ js/starmap.js
@@ -6,8 +6,7 @@ var nodes;
 
 var position = [ 0, 0 ];
 
-var spinning = 0;
-var highlighted_star;
+var stars_moving = 0;
 
 var SPIN_TIME = 1000;
 
@@ -19,9 +18,42 @@ var RFACTOR = .9;
 var CURSOR_RADIUS = 13;
 var CURSOR_XY = CURSOR_RADIUS / 1.414213562;
 
+var DOMAINS = {
+    "ra": { "min": 0, "max": 6.283 },
+    "dec": { "min": 1.57, "max": -1.57 },
+    "magnitude": { "min": 26, "max": -2  },
+    "absmagnitude": { "min": 18, "max": -11 },
+    "distance": { "min": 0, "max": 6100 },
+    "colourindex": { "min":  -0.274, "max": 2.994 },
+    "id": { "min": 0, "max": 403 }
+};
+
+var MARGIN = 50;
+
+var CIRCLE_MIN = 1;
+var CIRCLE_MAX = 15;
+
+var CIRCLE_FUNCS = {
+    "magnitude": d3.scale.sqrt()
+        .domain([13, -1.5]).range([1, 15]).clamp(true),
+    "absmagnitude": d3.scale.pow().exponent(0.333)
+        .domain([17, -10]).range([1, 10]).clamp(true)
+};
+
+var star_size_parameter = "magnitude";
+
 var history = [];
 
-var current_star = false;
+// three states: sphere with
+
+
+var state = 'sphere';
+
+var centre_star = false;
+
+
+
+// General 3D rotation functions
 
 function rad2deg(rad) {
     return 180 * rad / Math.PI;
@@ -43,11 +75,9 @@ function rotate2(vect2, theta) {
 
 function rotate3(vect3, ra, dec) {
     var rvect3 = [ 0, 0, 0 ];
-    
     var vect2 = rotate2([ vect3[0], vect3[1] ], ra);
-    
-    rvect3 = [ vect2[0], vect2[1], vect3[2] ];
 
+    rvect3 = [ vect2[0], vect2[1], vect3[2] ];
     vect2 = rotate2( [ rvect3[0], rvect3[2] ], dec );
     
     rvect3[0] = vect2[0];
@@ -57,10 +87,25 @@ function rotate3(vect3, ra, dec) {
 }
 
 
-function projection_isometric(d, coords) {
+
+
+function set_star_size_parameter(par) {
+    if( par != star_size_parameter ) {
+        var mag_f = CIRCLE_FUNCS[par];
+        if( mag_f ) {
+            d3.selectAll("circle.star")
+                .attr("r", function(d) { return mag_f(d[par]); } );
+            star_size_parameter = par
+        }
+    }
+}
+
+
+function star_sphere(d, coords) {
+
     var rvect = rotate3(
-	[ d.vector.x, d.vector.y, d.vector.z ],
-	coords[0], coords[1]
+	    [ d.vector.x, d.vector.y, d.vector.z ],
+	    deg2rad(coords[0]), deg2rad(coords[1])
     );
     x = cx + R * rvect[1];
     y = cy + R * rvect[2];
@@ -68,109 +113,7 @@ function projection_isometric(d, coords) {
     d.x = x;
     d.y = y;
     d.z = z;
-    
-    return "translate(" + x + "," + y + ")";
-}
-
-
-function magnitude_f(d) {
-    var size = 10 / Math.sqrt(1.5 + d.magnitude * .5);
-    if ( size < 2 ) {
-	    size = 2;
-    }
-    return size;
-}
-
-
-function centre_star(sname) {
-    var star = find_star(sname.toUpperCase());
-    if( star ) {
-	nodes.attr("transform", function(d) {
-	    return projection_isometric(d, -star.ra, -star.dec)
-	})
-
-    }
-
-}
-
-function find_star(sname) {
-    for ( var i = 0; i < stars.length; i++ ) {
-	
-	    if( stars[i].name == sname ) {
-	        return stars[i];
-	    }
-    }
-}
-
-
-function select_star(star, spin_time) {
-
-    gc_interp = rad2deginterp(position, [ -star.ra, -star.dec ]);
-    
-    // var gc_dist = d3.geo.distance(position, [ -star.ra, -star.dec ]);
-    var duration = 0;
-
-    duration = spin_time;
-
-    spinning = 1;
-    current_star = star;
-    
-    if( highlighted_star ) {
-        star_cursor(highlighted_star, 0);
-    }
-    $("div#about").hide();
-    $(".pointer").hide();
-
-    nodes.transition()
-	    .duration(duration)
-	    .attrTween("transform", function(d, i, a) {
-	        return select_tween(d, gc_interp)
-	    });
-    
-    d3.selectAll("circle.star")
-	    .transition()
-	    .duration(duration)
-	    .styleTween("opacity", function(d, i, a) {
-	        return hide_tween(d, gc_interp)
-	    })
-	    .each("end", function(e) {
-	        d3.select(this).each(function(d, i) {
-		        if( d.id == star.id ) {
-                    hide_star_text();
-		            star_cursor(this, 1);
-		            show_star_text(d);
-		            spinning = 0;
-                    $(".pointer").show();
-		        }
-	        });
-	    });
-    
-    position = [ -star.ra, -star.dec ];
-}
-
-function rad2deginterp(a, b) {
-    var interp = d3.geo.interpolate(
-	    [ rad2deg(a[0]), rad2deg(a[1]) ],
-	    [ rad2deg(b[0]), rad2deg(b[1]) ]
-    );
-    
-    return function(t) {
-	    var p = interp(t);
-	    return [ deg2rad(p[0]), deg2rad(p[1]) ];
-    }
-}
-
-function select_tween(d, gc_interp) {
-    return function(t) {
-	    return projection_isometric(d, gc_interp(t))
-    }
-}
-
-function hide_tween(d, gc_interp) {
-    return function(t) {
-	projection_isometric(d, gc_interp(t));
-	return star_opacity(d);
-    }
+    return d;
 }
 
 
@@ -185,13 +128,222 @@ function star_opacity(d) {
 }
 
 
-function star_cursor(elt, h) {
-    //var id = '#' + elt.id;
-    //d3.select(id).classed("highlight", h);
-    if( h ) {
-        highlighted_star = elt;
-    } 
+
+// generalised transition function.
+//
+// interp_f(d) - interpolator factory function: returns a tween
+//               function (t => the star datum) for a given star
+
+function stars_transition(interp_f, duration, after_f) {
+
+    stars_moving = 1;
+    
+    nodes.transition()
+	    .duration(duration)
+	    .attrTween("transform", function(d, i, a) {
+            var startween = interp_f(d);
+            return function(t) {
+                var s = startween(t);
+                return "translate(" + s.x + "," + s.y + ")";
+            }
+        });
+
+    d3.selectAll("circle.star")
+	    .transition()
+	    .duration(duration)
+	    .styleTween("opacity", function(d, i, a) {
+            var startween = interp_f(d);
+            return function(t) {
+                var s = startween(t);
+                return star_opacity(s);
+            }
+        })
+	    .each("end", after_f);
+ 
+    stars_moving = 0;
 }
+
+
+
+function select_star(star, spintime) {
+
+    var start, finish;
+
+    if( centre_star ) {
+        start = [ centre_star.ra, centre_star.dec ];
+    } else {
+        start = [ 0, 0 ];
+    }
+
+    if( star ) {
+        finish = [ star.ra, star.dec ];
+    } else {
+        finish = [ 0, 0 ];
+    }
+
+    // tween_f = tween factory: for each star returns a tween
+    // function t => star datum
+
+    var tween_f;
+
+    if( state == 'sphere' ) {
+        // If we're already in the sphere, calculate great-circle 
+        // paths so that the 3D illusion works
+
+        var great_circle = d3.geo.interpolate(
+	        [ rad2deg(-start[0]), rad2deg(-start[1]) ],
+	        [ rad2deg(-finish[0]), rad2deg(-finish[1]) ]
+        );
+        tween_f = function(d) {
+            return function(t) {
+                return star_sphere(d, great_circle(t));
+            }
+        };
+    } else {
+        // We are starting from a plot, so we don't need to calculate
+        // great-circle paths.
+        tween_f = function(d) {
+            var s = { "x": d.x, "y": d.y, "z": d.z };
+            var f = star_sphere(d, [ rad2deg(-finish[0]), rad2deg(-finish[1]) ] );
+            return d3.interpolate(s, f);
+        }
+    }
+
+    state = 'sphere';
+    centre_star = star;
+    
+    $("div#about").hide();
+    $(".pointer").hide();
+
+    stars_transition(tween_f, spintime, function(e) {
+	    d3.select(this).each(function(d, i) {
+		    if( d.id == star.id ) {
+                hide_star_text();
+		        show_star_text(d);
+                $(".pointer").show();
+		    }
+	    });
+	});
+    
+}
+
+// shortcut to return to the sphere after the user clicks the go-away button
+// on the plot controls
+
+function close_plot() {
+    select_star(centre_star, 1000);
+}
+
+
+//// render_plot(plot_f);
+//
+// Transition all stars to a scatterplot - plot_f(d) takes a star and
+// resets its datum x, y, z.  Uses each star's initial x, y, z as the
+// starting point.
+
+
+function render_plot(xparm, yparm, xrange, yrange) {
+
+    // tween_f = tween factory: for each star returns a tween
+    // function t => star datum
+    
+    var spintime = 1000;
+    
+    var plot_f = make_plot_f(xparm, yparm, xrange, yrange);
+
+    // a straight tween between wherever each star is, and 
+    // where we want it to be on the plot
+
+    var tween_f = function(d) {
+        var start = { "x": d.x, "y": d.y, "z": d.z };
+        var finish = plot_f(d);
+        return d3.interpolate(start, finish);
+    }
+
+    if( state == 'sphere' ) {
+        $("div#about").hide();
+        $(".pointer").hide();
+    }
+
+    state = 'plot';
+
+    stars_transition(tween_f, spintime, function(e) {
+	    d3.select(this).each(function(d, i) {
+            var p3 = plot_f(d);
+            d.x = p3.x;
+            d.y = p3.y;
+            d.z = p3.z
+	    });
+    });
+
+}
+
+
+
+function make_plot_f(xparm, yparm, xrange, yrange) {
+    var xd = DOMAINS[xparm];
+    var yd = DOMAINS[yparm];
+    var xscale = d3.scale.linear()
+        .domain([xd.min, xd.max])
+        .range([MARGIN, xrange - MARGIN]);
+    var yscale = d3.scale.linear()
+        .domain([yd.min, yd.max])
+        .range([yrange - MARGIN, MARGIN]);
+
+    return function(d) {
+        return {
+            "x": xscale(d[xparm]),
+            "y": yscale(d[yparm]),
+            "z": 100
+        }
+    };
+}
+
+
+function testplot(d) {
+    return {
+        "x": 50 + d.magnitude * 40,
+        "y": 10 + (d.name.charCodeAt(0) - 65) * 30,
+        "z": 100
+    };
+}
+
+// absmag (-10.9 to 14) - colourindex ( -0.274 - 2.994 )
+
+function test_hr_plot(d) {
+    return {
+        "x": 100 + d.colourindex * 240,
+        "y": 360 + d.absmagnitude * 21,
+        "z": 100
+    };
+}
+
+
+// distance from Sol
+
+// colourindex ( -0.274 - 2.994 )
+
+function test_hr_plot(d) {
+    return {
+        "x": 100 + d.colourindex * 240,
+        "y": 360 + d.absmagnitude * 21,
+        "z": 100
+    };
+}
+
+
+///// Calibration 
+
+
+
+
+
+
+
+
+///// Functions for displaying/hiding the star text
+//
+//
 
 function show_star_text(d) {
     $("div#text").removeClass("O B A F G K M C P W S start");
@@ -320,17 +472,21 @@ function render_map(elt, w, h, gostar) {
     	.append("g")
     	.attr("transform",
     	      function(d) {
-    		      return projection_isometric(d, [0, 0])
+    		      var s = star_sphere(d, [0, 0]);
+                  return "translate(" + s.x + "," + s.y + ")";
     	      });
     
+    var mag = CIRCLE_FUNCS["magnitude"]; 
+
     nodes.append("circle")
-    	.attr("r", magnitude_f)
+    	.attr("r", function(d) { return mag(d.magnitude) } )
     	.attr("class", function(d) { return "star " + d.class } )
         .attr("id", function(d, i) { return "circle_" + i })
     	.style("opacity", star_opacity)
     	.on("click", function(d) {
-    	    if( !spinning && d.z > -STAR_THRESHOLD ) {
+    	    if( !stars_moving && d.z > -STAR_THRESHOLD ) {
     		    select_star(d, SPIN_TIME);
+                $("div#plots").hide();
     		    d3.event.stopPropagation();
     	    }
     	});
